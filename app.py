23cc439dc2eb147efd088ae90d35805c8ba5f682
Muhammad Rehan Aslam <rjrajreehan@users.noreply.github.com>
2023-06-11 16:12:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ Shown are the stock **closing price** and **volume**.
 """)
 
 def user_input_features() :
-    stock_symbol = st.sidebar.selectbox('Symbol',('NATF.KA', 'COLG.KA'))
+    stock_symbol = st.sidebar.selectbox('Symbol',('NATF_KA', 'COLG_KA'))
     date_start = st.sidebar.date_input("Start Date", datetime.date(2015, 5, 31))
     date_end = st.sidebar.date_input("End Date", datetime.date.today())
 
